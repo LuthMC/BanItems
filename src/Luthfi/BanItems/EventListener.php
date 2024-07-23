f1ded8abe74b463c1d334b8a42d42ca6d2e5168a
LuthMC <luthfi366822@gmail.com>
2024-07-23 23:46:31 +0700

    Update EventListener.php

--- src/Luthfi/BanItems/EventListener.php
+++ src/Luthfi/BanItems/EventListener.php
@@ -6,9 +6,6 @@ namespace Luthfi\BanItems;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\event\player\PlayerPickupItemEvent;
-use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
 use pocketmine\player\Player;
 
@@ -27,28 +24,6 @@ class EventListener implements Listener {
         if ($this->isBanned($item)) {
             $event->cancel();
             $message = str_replace("{item}", $item->getName(), $this->plugin->getPluginConfig()->get("messages")["cannot-use-message"]);
-            $player->sendMessage($message);
-        }
-    }
-
-    public function onPlayerItemHeld(PlayerItemHeldEvent $event): void {
-        $player = $event->getPlayer();
-        $item = $event->getItem();
-
-        if ($this->isBanned($item)) {
-            $player->getInventory()->remove($item);
-            $message = str_replace("{item}", $item->getName(), $this->plugin->getPluginConfig()->get("messages")["ban-message"]);
-            $player->sendMessage($message);
-        }
-    }
-
-    public function onPlayerPickupItem(PlayerPickupItemEvent $event): void {
-        $player = $event->getPlayer();
-        $item = $event->getItem()->getItem();
-
-        if ($this->isBanned($item)) {
-            $event->cancel();
-            $message = str_replace("{item}", $item->getName(), $this->plugin->getPluginConfig()->get("messages")["ban-message"]);
             $player->sendMessage($message);
         }
     }
